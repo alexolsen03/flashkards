c6b553701d6084a7f44dfa0c84eeb246c2b155f2
alex <alexolsen03@gmail.com>
2014-05-12 22:16:15 -0500

    round 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,47 +18,50 @@
 $(document).ready(function(){
 
 	// $("#unlockIcon").click(function(){
-	// 	$("#unlockIcon").toggleClass(function(){
-	// 		if($(this).hasClass("locked")){
-	// 			$(this).removeClass("locked");
-	// 			$(this).addClass("unlocked");
-	// 		}else{
-	// 			$(this).addClass("locked");
-	// 			$(this).removeClass("unlocked");
-	// 		}
-	// 	});
+	// 	if($(this).hasClass('locked')){
+	// 		$(this).removeClass('locked');
+	// 		$(this).addClass('unlocked');
+	// 		$("#leftPanel").removeClass('lock-panel');
+	// 	}else{
+	// 		$(this).removeClass('unlocked');
+	// 		$(this).addClass('locked');
+	// 		$("#leftPanel").addClass('lock-panel');
+	// 	}
 	// });
-	$("#unlockIcon").click(function(){
-		if($(this).hasClass('locked')){
-			$(this).removeClass('locked');
-			$(this).addClass('unlocked');
-			$("#leftPanel").removeClass('lock-panel');
-		}else{
-			$(this).removeClass('unlocked');
-			$(this).addClass('locked');
-			$("#leftPanel").addClass('lock-panel');
+
+	// $("#cardNumList li").click(function(){
+	// 	var amt = $(this).find(".card-amt").text();
+	// 	$("#numOptionsItemSelected").find(".card-amt").text(amt);
+	// 	var answers = $("#answerOptions");
+	// 	$("#answerOptions").empty();
+	// 	for(var i = 0; i < amt; i++){
+	// 		$("#answerOptions").append("<li class='card-in'><h1>TESTER</h1></li>");
+	// 	}
+	// });
+
+	$("#modeSubmenu li").click(function(){
+		var val = $(this).find("h2").text();
+		if(val == "Matching"){
+			$("#matchMode").css("display", "block");
+			$("#standardMode").css("display", "none");
+		}
+		if(val == "Standard"){
+			$("#standardMode").css("display", "block");
+			$("#matchMode").css("display", "none");
 		}
 	});
 
-	// $("#cardNumList").hover(function(){
-	// 	if(!$(this).hasClass("hover-in")){
-	// 		$(this).removeClass("hover-out").addClass("hover-in");
-	// 	}
-	// },function(){
-	// 	if(!$(this).hasClass("hover-out")){
-
-	// 		$(this).removeClass("hover-in").addClass("hover-out");
-	// 	}
-	// });
-
-	$("#cardNumList li").click(function(){
-		var amt = $(this).find(".card-amt").text();
-		$("#numOptionsItemSelected").find(".card-amt").text(amt);
-		var answers = $("#answerOptions");
-		$("#answerOptions").empty();
-		for(var i = 0; i < amt; i++){
+	$("#numOfCardsList li").click(function(){
+		var amt = $(this).attr('val');
+		var numSel = $("#numSelWrapper");
+		$(numSel).empty();
+		$('#answerOptions').empty();
+		var str = "";
+		for(var i=0; i<amt; i++){
 			$("#answerOptions").append("<li class='card-in'><h1>TESTER</h1></li>");
+			str += "<i class='fa fa-square-o'></i>";
 		}
+		$(numSel).append(str);
 	});
 
 });
