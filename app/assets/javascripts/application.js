f394c369ad32357346e6e54991c8751a09c815e0
alex <alexolsen03@gmail.com>
2014-05-14 21:54:35 -0500

    matching options

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,12 +43,23 @@ $(document).ready(function(){
 		var val = $(this).find("h2").text();
 		if(val == "Matching"){
 			$("#matchMode").css("display", "block");
+			$("#matchingSelWrapper").css("display", "block");
+			$("#matchingOptionsSubmenu").css("display", "block");
+
 			$("#standardMode").css("display", "none");
+			$("#numSelWrapper").css("display", "none");
+			$("#numOptionsSubmenu").css("display", "none");
 		}
 		if(val == "Standard"){
 			$("#standardMode").css("display", "block");
+			$("#numSelWrapper").css("display", "block");
+			$("#numOptionsSubmenu").css("display", "block");
+
 			$("#matchMode").css("display", "none");
+			$("#matchingSelWrapper").css("display", "none");
+			$("#matchingOptionsSubmenu").css("display", "none");
 		}
+		$("#currentMode").text(val);
 	});
 
 	$("#numOfCardsList li").click(function(){
@@ -62,6 +73,26 @@ $(document).ready(function(){
 			str += "<i class='fa fa-square-o'></i>";
 		}
 		$(numSel).append(str);
+	});
+
+	$("#matchingOptionsList li").click(function(){
+		var amt = $(this).attr('val');
+		var match = $("#matchMode");
+		$(match).empty();
+		for(var i=0; i<amt; i++){
+			var str ="<div class='row'>";
+
+			for(var n=0; n<amt; n++){
+				str+=(	"<div class='col"+(n+1)+" col'>" +
+					"	<div class='match-option'>Man</div>" +
+					"</div>");
+			}
+
+			str+="</div>";
+
+			$(match).append(str);
+
+		}
 	});
 
 });
